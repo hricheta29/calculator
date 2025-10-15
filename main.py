4066f9294b522f16c3f1330df98ba479c501baf2
hricheta29 <hricheta@gmail.com>
2025-10-15 22:22:25 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def calculator(a,b,op):
+    if op==1:
+        return(a+b)
+    elif op==2:
+        return(a-b)
+    elif op==3:
+        return(a*b)
+    elif op==4:
+        return("a/b")
+
+print(calculator(10,2,2))
