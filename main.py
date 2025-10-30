cb5d08892b28191d0927fc4c85cfc0e03220c13b
hricheta29 <hricheta@gmail.com>
2025-10-30 21:42:02 +0530

    First commit

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-def calculator(a,b,op):
-    if op==1:
-        return(a+b)
-    elif op==2:
-        return(a-b)
-    elif op==3:
-        return(a*b)
-    elif op==4:
-        return("a/b")
+import requests
 
-print(calculator(10,2,2))
+API_KEY = "4e57fc58c00641f1a8b155647253010 "  # you’ll get it free from weatherapi.com
+city = "Kolkata"
+
+url = f"https://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}"
+
+response = requests.get(url)
+data = response.json()
+
+print("City:", data["location"]["name"])
+print("Temperature:", data["current"]["temp_c"], "°C")
+print("Condition:", data["current"]["condition"]["text"])
